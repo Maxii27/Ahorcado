502e8d35657d62391e1e5817f3ae96dea0e22a73
Maxii27 <oscarpereyra2003@gmail.com>
2023-09-08 22:00:28 -0300

    Cambios

--- ahorcado/ahorcado.js
+++ ahorcado/ahorcado.js
@@ -2,7 +2,7 @@
 
 
 var palabras = [["Api", "Iterfasz entre usuario y servidor"],
- ["Programador", "Diseñador de paginas web"],
+ ["Desarollo", "Diseñador de paginas mobile"],
  ["github", "Herramienta para crear una branch"],
   ["Facebook", "Red Social mas conocida"], 
  ["html", "Esqueleto de una pagina web"], 
