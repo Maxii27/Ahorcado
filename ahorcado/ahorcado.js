739a72292380a119288cf5639b9b49c2e08bb69d
Maxii27 <oscarpereyra2003@gmail.com>
2023-09-13 15:47:38 -0300

    cambio7

--- ahorcado/ahorcado.js
+++ ahorcado/ahorcado.js
@@ -3,7 +3,7 @@
 
 var palabras = [["Api", "Cristina chorra"],
  ["Auto", "representantes"],
- ["Mediafire", "Herramienta para crear una branch"],
+ ["yahoo", "Herramienta para crear una branch"],
   ["Instagram", "Red Social mas conocida"], 
  ["html", "Esqueleto de una pagina web"], 
  ["Algoritmos", "Series de pasos que se ejecutan para resolver un tipo de problema"], 
