e2f4ae022941219fc555bc3916482735be9b0696
AnselmoValenzuela <valenzuela.anselmo71@gmail.com>
2023-09-13 11:37:23 -0300

    Cambios

--- ahorcado/ahorcado.js
+++ ahorcado/ahorcado.js
@@ -2,7 +2,7 @@
 
 
 var palabras = [["Api", "Iterfasz entre usuario y servidor"],
- ["Diseñador", "Se encarga de la usabilidad de un software"],
+ ["Programador", "Diseñador de paginas web"],
  ["github", "Herramienta para crear una branch"],
   ["Facebook", "Red Social mas conocida"], 
  ["html", "Esqueleto de una pagina web"], 
